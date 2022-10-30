fb72107e76085df30a162f5b747588d81753c9f3
Anderson Job Loeffler <aloeffler.j@gmail.com>
2022-10-30 01:56:44 -0300

    :sparkles: feat: adds simple redirect to frontend application

--- src/pages/content/index.ts
+++ src/pages/content/index.ts
@@ -1,7 +1,17 @@
-console.log("content loaded");
+console.log('content loaded');
 
 /**
  * @description
  * Chrome extensions don't support modules in content scripts.
  */
-import("./components/Demo");
+import('./components/Demo');
+import { api } from '@src/services/api';
+import axios from 'axios';
+import { EnvironmentConfig } from '@src/config/environmentConfig';
+const landingPage = EnvironmentConfig.mainClientApiBasePath;
+
+switch (window.location.hostname) {
+  case 'www.instagram.com':
+    location.replace(`${landingPage}/tasks`);
+    break;
+}
