8affef8fbfb260e2825d8992bd15cfee11ed96a8
Anderson Job Loeffler <aloeffler.j@gmail.com>
2022-10-30 00:33:05 -0300

    :ok_hand: style: runs lint to turn quotes to single quotes

--- src/pages/content/components/Demo/index.tsx
+++ src/pages/content/components/Demo/index.tsx
@@ -1,11 +1,13 @@
-import { createRoot } from "react-dom/client";
-import App from "@src/pages/content/components/Demo/app";
-import refreshOnUpdate from "virtual:reload-on-update-in-view";
+import { createRoot } from 'react-dom/client';
+import App from '@src/pages/content/components/Demo/app';
+import refreshOnUpdate from 'virtual:reload-on-update-in-view';
 
-refreshOnUpdate("pages/content/components/Demo");
+refreshOnUpdate('pages/content/components/Demo');
 
-const root = document.createElement("div");
-root.id = "chrome-extension-boilerplate-react-vite-content-view-root";
-document.body.append(root);
+if (location.port !== '8614') {
+  const root = document.createElement('div');
+  root.id = 'chrome-extension-boilerplate-react-vite-content-view-root';
+  document.body.append(root);
 
-createRoot(root).render(<App />);
+  createRoot(root).render(<App />);
+}
